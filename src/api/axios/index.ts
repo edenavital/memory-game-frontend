0756ce61aad45732e2b5e99a3adca648c98e6d4c
Eden Avital <edendor@gmail.com>
2023-06-10 13:42:09 +0300

    update base url of axios

--- src/api/axios/index.ts
+++ src/api/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // process.env.NODE_ENV === "production"
